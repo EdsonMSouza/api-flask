1d0586ad974dce558ae134344555b97bef4f6c97
EdsonMSouza <prof.edson.melo@gmail.com>
2023-04-09 21:17:18 -0300

    Update Repo

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# api-flask
+# API FLASK
+
+This API aims to present a brief to consume an API resources, mainly for students in the early years of Computer Science courses and related. For this reason, it has few EndPoints (resources) to use, and can be expanded according to the need. Details of this project can be accessed below.
+
+Esta API tem como objetivo apresentar um breve consumo de recursos de uma API, principalmente para alunos dos anos iniciais dos cursos de Ciência da Computação e correlatos. Por isso, possui poucos EndPoints (recursos) para utilizar, podendo ser expandido conforme a necessidade. Os detalhes deste projeto podem ser acessados.
---
+++ repl.it.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from flask import Flask, jsonify
+
+# inicializa o Flask
+app = Flask(__name__)
+
+# Criando uma rota para acesso ao site (aplicação)
+@app.route('/') # decorator (define uma rota)
+def homepage():
+  retorno = 'A API está online.</br>'
+  retorno += '<h3>Métodos da API</h3>'
+  retorno += '<em>/somar/v1/v2</em> - Retorna a soma entre dois valores v1 e v2</br>'
+  retorno += '<em>/multiplicar/v1/v2</em> - Retorna a multiplicação entre dois valores v1 e v2</br>'
+  retorno += '<em>/previa</em> - Retorna os cinco primeiros registros de um <em>dataset</em> sobre vendas</br>' 
+  return retorno
+  
+# Construindo duas funcionalidades de calculadora (somar e multiplicar)
+# Somar
+@app.route('/somar/<string:v1>/<string:v2>')
+def somar(v1, v2):
+  retorno = {'operacao':'adição', 'v1':v1, 'v2':v2,'resultado':int(v1)+int(v2)}
+  return jsonify(retorno)
+
+# Multiplicar
+@app.route('/multiplicar/<string:v1>/<string:v2>')
+def multiplicar(v1, v2):
+  retorno = {'operacao':'multiplicação', 'v1':v1, 'v2':v2,'resultado':int(v1)*int(v2)}
+  return jsonify(retorno)
+
+# Acessando dados de um dataset (Pandas)
+# Listando os cinco primeiros registros da tabela
+@app.route('/previa') 
+def previa():
+  dados = pd.read_csv('https://github.com/EdsonMSouza/api-flask/vendas.csv')
+  resposta = dados.to_dict(orient="list")
+  return jsonify(resposta)
+  
+# Acessando dados de um dataset (Pandas)
+# Mostrando o Total de Vendas do Dataset
+@app.route('/totalvendas')
+def vendas():
+  dados = pd.read_csv('https://github.com/EdsonMSouza/api-flask/vendas.csv')
+  resposta = {'total_vendas':dados.VENDAS.sum()}
+  return jsonify(resposta)
+  
+# executando a aplicação de modo aberto (web)
+app.run('0.0.0.0', debug=True)
